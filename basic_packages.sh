acd106a73f20d0c11f726d046302d6ce305c7495
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2021-12-30 03:57:08 +0100

    basic packages (complete afaik)

---
+++ basic_packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x #echo on
+
+pacman -Syu --noconfirm fish nano vim micro git wget kget nvidia nvidia-settings opencl-nvidia lib32-opencl-nvidia nvidia-utils lib32-nvidia-utils mesa xf86-video-intel
+
+exit0
