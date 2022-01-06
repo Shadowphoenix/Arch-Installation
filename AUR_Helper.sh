33ebfcf398426128cc7226a56188c94e7864d4be
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-01-06 06:19:04 +0100

    updates v2

--- AUR_Helper.sh
+++ AUR_Helper.sh
@@ -1,20 +1,17 @@
+#!/bin/bash
 #execute script as a user, makepkg executed as root causes hickups
 #Using YetAnotherYogurt/yay as CLI-AUR-Helper to install Pamac
 
-#!/bin/bash
-
 set -x #echo on
 
+#installing yay
 cd /tmp
-
 sudo git clone https://aur.archlinux.org/yay-git.git 
-
 sudo chown -R neko ./yay-git #Own Buildirectory to user for full access (modify user as needed)
-
 cd ./yay-git
-
 makepkg -si #Building Yay
 
+#installing pamac
 sudo yay -Syu pamac-aur #Installing Pamac + Dependencies
 
 exit0
