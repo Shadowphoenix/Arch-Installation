3d2deedd5cfc55cafe4f43c391d6cb06712cca00
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-01-24 04:05:22 +0100

    Add files via upload

---
+++ AUR-Packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x #echo on
+
+yay -Syu davinci-resolve isw optimus-manager-git optimus-manager-qt-git ytmdesktop-bin betterdiscord-installer-bin bottles-git dxvk-bin lib32-libdxvk libdxvk
+
+exit0
