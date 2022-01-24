e3ce3fc0f1314a5fcd9334d345ea6a6a52223afc
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-01-24 09:18:23 +0100

    Update and rename fusuma_user.sh to fusuma_user_prep.sh

---
+++ fusuma_user_prep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x #echo on
+
+#adding rubygems to $PATH
+echo 'export PATH="$PATH:/home/neko/.local/share/gem/ruby/3.0.0/bin"' >> ~/.bashrc
+
+#prerequisites
+sudo gpasswd -a $USER input
+newgrp input
