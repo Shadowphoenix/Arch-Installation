344de149bbeada70ace59417f9d952ada12cc413
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-01-24 08:30:12 +0100

    Update KDE_root.sh

--- KDE_root.sh
+++ KDE_root.sh
@@ -2,7 +2,7 @@
 
 set -x #echo on
 
-pacman -Syu plasma-desktop sddm sddm-kcm kate konsole dolphin dolphin-plugins partitionmanager spectacle latte-dock libreoffice-fresh discord firefox sweeper kdeconnect plasma-systemmonitor powerdevil filelight kcron firewalld neofetch lolcat discover packagekit-qt5 fwupd obs-studio kdeplasma-addons purpose qt5-webengine quota-tools plasma-nm kmix kde-gtk-config
+pacman -Syu plasma-desktop sddm sddm-kcm kate konsole dolphin dolphin-plugins partitionmanager spectacle latte-dock sweeper kdeconnect plasma-systemmonitor powerdevil filelight kcron firewalld neofetch lolcat discover packagekit-qt5 fwupd kdeplasma-addons purpose qt5-webengine quota-tools plasma-nm kmix kde-gtk-config
 
 systemctl enable NetworkManager
 systemctl enable sddm
