66f797101b84ab21268be97eaa049a23d540f4ae
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-01-24 06:28:40 +0100

    Update and rename default_fish_WIP.sh to default_fish.sh

---
+++ default_fish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x #echo on
+
+chsh -s /bin/fish
+
+exit0
--- default_fish_WIP.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x #echo on
-
-chsh -s /bin/fish
-
-sudo echo 'neofetch | lolcat' >> /etc/fish/config.fish
-
-exit0
