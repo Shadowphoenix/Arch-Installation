b669b8e608cf395fdadfe9a46d51492adc674068
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-01-24 09:36:52 +0100

    Update AUR-Packages_user.sh

--- AUR-Packages_user.sh
+++ AUR-Packages_user.sh
@@ -4,4 +4,4 @@ set -x #echo on
 
 yay -Syu davinci-resolve isw optimus-manager-git optimus-manager-qt-git ytmdesktop-bin betterdiscord-installer-bin bottles-git dxvk-bin lib32-libdxvk libdxvk
 
-exit0
+systemctl enable OptimusManager
