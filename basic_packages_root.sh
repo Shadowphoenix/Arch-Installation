cb5840c4a58ec2f5c12b1744b6c95b6e50cc8c11
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-01-24 06:28:09 +0100

    Update basic_packages_root.sh

--- basic_packages_root.sh
+++ basic_packages_root.sh
@@ -2,6 +2,6 @@
 
 set -x #echo on
 
-pacman -Syu fish nano vim micro git wget kget nvidia nvidia-settings opencl-nvidia lib32-opencl-nvidia nvidia-utils lib32-nvidia-utils mesa xf86-video-intel
+pacman -Syu fish nano git wget kget nvidia nvidia-settings opencl-nvidia lib32-opencl-nvidia nvidia-utils lib32-nvidia-utils mesa xf86-video-intel
 
 exit0
