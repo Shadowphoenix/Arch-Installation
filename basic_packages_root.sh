a471a6c296e354fca68da5982234b1ea59671dc6
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-01-25 01:38:22 +0100

    Update basic_packages_root.sh

--- basic_packages_root.sh
+++ basic_packages_root.sh
@@ -2,7 +2,7 @@
 
 set -x #echo on
 
-pacman -Syu fish nano git wget kget nvidia nvidia-settings opencl-nvidia nvidia-utils mesa xf86-video-intel plasma-desktop sddm sddm-kcm kate konsole dolphin dolphin-plugins partitionmanager spectacle latte-dock sweeper kdeconnect plasma-systemmonitor powerdevil filelight kcron firewalld neofetch lolcat discover packagekit-qt5 fwupd kdeplasma-addons purpose qt5-webengine quota-tools plasma-nm kmix kde-gtk-config firefox ntfs-3g exfatprogs kgamma5
+pacman -Syu fish nano wget kget nvidia nvidia-settings opencl-nvidia nvidia-utils mesa xf86-video-intel plasma-desktop sddm sddm-kcm kate konsole dolphin dolphin-plugins partitionmanager spectacle latte-dock sweeper kdeconnect plasma-systemmonitor powerdevil filelight kcron firewalld neofetch lolcat discover packagekit-qt5 fwupd kdeplasma-addons purpose qt5-webengine quota-tools plasma-nm kmix kde-gtk-config firefox ntfs-3g exfatprogs kgamma5
 
 systemctl enable NetworkManager
 systemctl enable sddm
