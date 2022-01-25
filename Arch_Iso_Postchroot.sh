e96de45d8e3acc0bc6a550398cbc0f2f421f22fa
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-01-25 01:35:48 +0100

    Update Arch_Iso_Postchroot.sh

--- Arch_Iso_Postchroot.sh
+++ Arch_Iso_Postchroot.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
-nano /etc/localtime
-echo arch > /etc/hostname
-echo LANG=de_DE.UTF-8 > /etc/locale.conf
-echo de_DE.UTF-8 > /etc/locale.gen
+hwclock --systohc
+echo "arch" > /etc/hostname
+nano /etc/hostname
+echo "LANG=de_DE.UTF-8" > /etc/locale.conf
+nano /etc/locale.conf
+echo "de_DE.UTF-8" >> /etc/locale.gen
+nano /etc/locale.gen
 locale-gen
 echo MEYMAP=de-latin1 > /etc/vconsole.conf
-cat /etc/hosts
+echo "127.0.0.1 localhost \n ::1  localhost \n 127.0.1.1 arch.localdomain arch" >> /etc/hosts
 nano /etc/hosts
 mkinitcpio -p linux
 passwd
@@ -15,6 +18,5 @@ pacman -Syu grub efibootmgr os-prober
 grub-install --target=x86_64-efi --efi-directory=/efi/ --bootloader-id=GRUB
 nano /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
-nano /etc/hosts
 useradd -G wheel,audio,video -m neko
 passwd neko
