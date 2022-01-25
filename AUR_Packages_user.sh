772bbf0ea8fc85ffd55b5e245df1639932b461ff
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-01-25 09:31:07 +0100

    Delete AUR_Packages_user.sh

--- AUR_Packages_user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x #echo on
-
-yay -Syu davinci-resolve isw optimus-manager-git optimus-manager-qt-git ytmdesktop-bin betterdiscord-installer-bin bottles-git dxvk-bin lib32-libdxvk libdxvk
-
-systemctl enable optimus-manager
