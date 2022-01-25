c70bb08d15e386994103c557785530f72b2cde21
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-01-25 09:24:31 +0100

    Update Arch_Iso_Postchroot.sh

--- Arch_Iso_Postchroot.sh
+++ Arch_Iso_Postchroot.sh
@@ -21,3 +21,11 @@ grub-mkconfig -o /boot/grub/grub.cfg
 useradd -d /home/neko -M neko
 chown -R neko:neko /home/neko
 passwd neko
+
+
+#installing SDDM + KDE + Videodrivers 
+
+pacman -Syu fish nano wget kget nvidia nvidia-settings opencl-nvidia nvidia-utils mesa xf86-video-intel plasma-desktop sddm sddm-kcm kate konsole dolphin dolphin-plugins partitionmanager spectacle latte-dock sweeper kdeconnect plasma-systemmonitor powerdevil filelight kcron firewalld neofetch lolcat discover packagekit-qt5 fwupd kdeplasma-addons purpose qt5-webengine quota-tools plasma-nm kmix kde-gtk-config firefox ntfs-3g exfatprogs kgamma5
+
+systemctl enable NetworkManager
+systemctl enable sddm
