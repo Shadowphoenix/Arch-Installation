162e4ff592f3389d31288d9e16f985e1979d2dc7
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-01-26 04:29:35 +0100

    Update fusuma_user_prep.sh

--- fusuma_user_prep.sh
+++ fusuma_user_prep.sh
@@ -4,6 +4,7 @@ set -x #echo on
 
 #adding rubygems to $PATH
 echo 'export PATH="$PATH:/home/neko/.local/share/gem/ruby/3.0.0/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:/root/.local/share/gem/ruby/3.0.0/bin"' >> ~/.bashrc
 
 #prerequisites
 sudo gpasswd -a $USER input
