4d24008175cbf817c78b471e5eca57ccc80a05ba
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-02-24 17:20:45 +0100

    Update fusuma_prep.sh

--- fusuma_prep.sh
+++ fusuma_prep.sh
@@ -6,7 +6,3 @@ set -x #echo on
 echo 'export PATH="$PATH:/home/neko/.local/share/gem/ruby/3.0.0/bin"' >> ~/.bashrc
 echo 'export PATH="$PATH:/root/.local/share/gem/ruby/3.0.0/bin"' >> ~/.bashrc
 nano ~/.bashrc
-
-#prerequisites
-sudo gpasswd -a $USER input
-newgrp input
