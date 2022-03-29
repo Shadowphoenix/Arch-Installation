7c34d7e3a579bd0f569490c623bacbf63f30b82b
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-03-29 19:41:01 +0200

    Update Postchroot.sh

--- Postchroot.sh
+++ Postchroot.sh
@@ -46,7 +46,9 @@ systemctl enable NetworkManager
 systemctl enable sddm
 
 #installing other stuff and winedependencies - enable Multilib beforehand
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 nano /etc/pacman.conf
+
 #sed 's,#[multilib] \nIt #Include = /etc/pacman.d/mirrorlist ,[multilib] \nIt Include = /etc/pacman.d/mirrorlist,' /etc/pacman.conf
 
 pacman -Syu --needed steam steam-native-runtime wine winetricks lib32-opencl-nvidia lib32-vulkan-intel wine-mono wine-gecko libreoffice-fresh discord obs-studio giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox lutris pycharm-community-edition ipython intellij-idea-community-edition lldb --noconfirm
