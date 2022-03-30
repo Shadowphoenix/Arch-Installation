770e88c27382256b2bf76a1f77fcb4fa12d5e6a9
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-03-30 05:43:39 +0200

    Update Postchroot.sh

--- Postchroot.sh
+++ Postchroot.sh
@@ -23,7 +23,7 @@ mkinitcpio -p linux
 passwd
 
 #Installing Grub
-pacman -Syu --needed grub efibootmgr os-prober --no-confirm
+pacman -Syu --needed grub efibootmgr os-prober --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
 
 #Uncomment "Disable OS-Prober" if multibooting
@@ -47,11 +47,11 @@ EDITOR=nano visudo
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 nano /etc/pacman.conf
 
-pacman -Syu --needed fish nano wget kget nvidia nvidia-settings opencl-nvidia nvidia-utils mesa xf86-video-intel plasma-desktop sddm sddm-kcm kate konsole dolphin dolphin-plugins partitionmanager spectacle latte-dock sweeper kdeconnect plasma-systemmonitor plasma-firewall powerdevil filelight kcron firewalld neofetch lolcat discover packagekit-qt5 fwupd kdeplasma-addons purpose qt5-webengine quota-tools plasma-nm kmix kde-gtk-config firefox ntfs-3g exfatprogs kgamma5 dialog wpa_supplicant ifplug --no-confirm
+pacman -Syu --needed fish nano wget kget nvidia nvidia-settings opencl-nvidia nvidia-utils mesa xf86-video-intel plasma-desktop sddm sddm-kcm kate konsole dolphin dolphin-plugins partitionmanager spectacle latte-dock sweeper kdeconnect plasma-systemmonitor plasma-firewall powerdevil filelight kcron firewalld neofetch lolcat discover packagekit-qt5 fwupd kdeplasma-addons purpose qt5-webengine quota-tools plasma-nm kmix kde-gtk-config firefox ntfs-3g exfatprogs kgamma5 dialog wpa_supplicant ifplug --noconfirm
 
 systemctl enable NetworkManager
 systemctl enable sddm
 
 #enabling multilib + installing wine and steam
 
-pacman -Syu --needed steam steam-native-runtime wine winetricks lib32-opencl-nvidia lib32-vulkan-intel wine-mono wine-gecko libreoffice-fresh discord obs-studio giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox lutris pycharm-community-edition ipython intellij-idea-community-edition lldb --no-confirm
+pacman -Syu --needed steam steam-native-runtime wine winetricks lib32-opencl-nvidia lib32-vulkan-intel wine-mono wine-gecko libreoffice-fresh discord obs-studio giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox lutris pycharm-community-edition ipython intellij-idea-community-edition lldb --noconfirm
