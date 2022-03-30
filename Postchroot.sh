c50ccd99232fb09c97676eaea53c8f9da44dc903
Shadowphoenix <38107036+Shadowphoenix@users.noreply.github.com>
2022-03-30 05:40:12 +0200

    Update Postchroot.sh

--- Postchroot.sh
+++ Postchroot.sh
@@ -23,7 +23,7 @@ mkinitcpio -p linux
 passwd
 
 #Installing Grub
-pacman -Syu --no-confirm --needed grub efibootmgr os-prober
+pacman -Syu --needed grub efibootmgr os-prober --no-confirm
 grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
 
 #Uncomment "Disable OS-Prober" if multibooting
@@ -33,7 +33,7 @@ sed -i 's,#GRUB_DISABLE_OS_PROBER=false,GRUB_DISABLE_OS_PROBER=false,' /etc/defa
 grub-mkconfig -o /boot/grub/grub.cfg
 
 #Creating user - Remember to edit as needed, especially if your /home is not on a separate partition and hence got wiped during reinstallation
-useradd -d /home/neko -G wheel,audio,video,input -M neko #redirecting the user to already existing homepartition
+useradd -G wheel,audio,video,input -M neko
 chown -R neko:neko /home/neko #User to own his ~/
 passwd neko #set pw for user
 
@@ -47,11 +47,11 @@ EDITOR=nano visudo
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 nano /etc/pacman.conf
 
-pacman -Syu --no-confirm --needed fish nano wget kget nvidia nvidia-settings opencl-nvidia nvidia-utils mesa xf86-video-intel plasma-desktop sddm sddm-kcm kate konsole dolphin dolphin-plugins partitionmanager spectacle latte-dock sweeper kdeconnect plasma-systemmonitor plasma-firewall powerdevil filelight kcron firewalld neofetch lolcat discover packagekit-qt5 fwupd kdeplasma-addons purpose qt5-webengine quota-tools plasma-nm kmix kde-gtk-config firefox ntfs-3g exfatprogs kgamma5 dialog wpa_supplicant ifplug
+pacman -Syu --needed fish nano wget kget nvidia nvidia-settings opencl-nvidia nvidia-utils mesa xf86-video-intel plasma-desktop sddm sddm-kcm kate konsole dolphin dolphin-plugins partitionmanager spectacle latte-dock sweeper kdeconnect plasma-systemmonitor plasma-firewall powerdevil filelight kcron firewalld neofetch lolcat discover packagekit-qt5 fwupd kdeplasma-addons purpose qt5-webengine quota-tools plasma-nm kmix kde-gtk-config firefox ntfs-3g exfatprogs kgamma5 dialog wpa_supplicant ifplug --no-confirm
 
 systemctl enable NetworkManager
 systemctl enable sddm
 
 #enabling multilib + installing wine and steam
 
-pacman -Syu --no-confirm --needed steam steam-native-runtime wine winetricks lib32-opencl-nvidia lib32-vulkan-intel wine-mono wine-gecko libreoffice-fresh discord obs-studio giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox lutris pycharm-community-edition ipython intellij-idea-community-edition lldb
+pacman -Syu --needed steam steam-native-runtime wine winetricks lib32-opencl-nvidia lib32-vulkan-intel wine-mono wine-gecko libreoffice-fresh discord obs-studio giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox lutris pycharm-community-edition ipython intellij-idea-community-edition lldb --no-confirm
